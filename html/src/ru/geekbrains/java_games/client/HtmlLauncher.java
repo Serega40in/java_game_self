fdfdbef48a30a83158c16fcf1ecf612f7ae07140
Serega40in <serega40-in@yandex.ru>
2017-08-15 11:33:49 +0300

    add all

---
+++ html/src/ru/geekbrains/java_games/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package ru.geekbrains.java_games.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import ru.geekbrains.java_games.JavaGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new JavaGame();
+        }
+}
