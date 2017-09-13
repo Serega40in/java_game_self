080068005a55333553aafa9d5a375876e50a4982
serega40in <serega40-in@yandex.ru>
2017-09-14 02:54:43 +0300

    DZ6

--- desktop/src/ru/geekbrains/java_games/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/java_games/desktop/DesktopLauncher.java
@@ -2,11 +2,17 @@ package ru.geekbrains.java_games.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.geekbrains.java_games.JavaGame;
+
+import ru.geekbrains.java_games.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new JavaGame(), config);
+//		float aspect = 480f / 854f;
+        float aspect = 3f / 4f;
+
+		config.height = 800;
+		config.width = (int)(config.height * aspect);
+		new LwjglApplication(new StarGame(), config);
 	}
 }
