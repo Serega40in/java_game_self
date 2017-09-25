8b206188b09ac03556b86c92f879b7a1227b0700
serega40in <serega40-in@yandex.ru>
2017-09-26 01:07:20 +0300

    the end

--- core/src/ru/geekbrains/java_games/common/enemies/EnemiesEmitter.java
+++ core/src/ru/geekbrains/java_games/common/enemies/EnemiesEmitter.java
@@ -11,13 +11,15 @@ import ru.geekuniversity.engine.utils.Regions;
 
 public class EnemiesEmitter {
 
+    private int stage;
+
     private final Rect worldBounds;
     private final EnemyPool enemyPool;
 
     private final Sound sndBullet;
     private final TextureRegion bulletRegion;
 
-    private static final float ENEMY_SMALL_HEIGHT = 0.08f;
+    private static final float ENEMY_SMALL_HEIGHT = 0.1f;
     private final TextureRegion[] enemySmallRegions;
     private final Vector2 enemySmallV = new Vector2(0f, -0.2f);
     private static final float ENEMY_SMALL_BULLET_HEIGHT = 0.01f;
@@ -44,11 +46,38 @@ public class EnemiesEmitter {
     private static final float ENEMY_BIG_RELOAD_INTERVAL = 1f;
     private static final int ENEMY_BIG_HP = 20;
 
+    private static final float ENEMY_PLANE_HEIGHT = 0.2f;
+    private final TextureRegion[] enemyPLANERegions;
+    private final Vector2 enemyPLANEV = new Vector2(0f, -0.25f);
+    private static final float ENEMY_PLANE_BULLET_HEIGHT = 0.05f;
+    private static final float ENEMY_PLANE_BULLET_VY = -0.9f;
+    private static final int ENEMY_PLANE_BULLET_DAMAGE = 20;
+    private static final float ENEMY_PLANE_RELOAD_INTERVAL = 3f;
+    private static final int ENEMY_PLANE_HP = 5;
+
+    private static final float ENEMY_STAR_HEIGHT = 0.1f;
+    private final TextureRegion[] enemySTARRegions;
+    private final Vector2 enemySTARV = new Vector2(0f, -0.15f);
+    private static final float ENEMY_STAR_BULLET_HEIGHT = 0.01f;
+    private static final float ENEMY_STAR_BULLET_VY = -2f;
+    private static final int ENEMY_STAR_BULLET_DAMAGE = 2;
+    private static final float ENEMY_STAR_RELOAD_INTERVAL = 0.5f;
+    private static final int ENEMY_STAR_HP = 7;
+
+    private static final float ENEMY_BIGPLANE_HEIGHT = 0.5f;
+    private final TextureRegion[] enemyBIGPLANERegions;
+    private final Vector2 enemyBIGPLANEV = new Vector2(0f, -0.001f);
+    private static final float ENEMY_BIGPLANE_BULLET_HEIGHT = 0.1f;
+    private static final float ENEMY_BIGPLANE_BULLET_VY = -0.01f;
+    private static final int ENEMY_BIGPLANE_BULLET_DAMAGE = 50;
+    private static final float ENEMY_BIGPLANE_RELOAD_INTERVAL = 9f;
+    private static final int ENEMY_BIGPLANE_HP = 25;
+
 
     private float generateInterval = 4f;
     private float generateTimer;
 
-    public EnemiesEmitter(EnemyPool enemyPool, Rect worldBounds, TextureAtlas atlas, Sound sndBullet) {
+    public EnemiesEmitter(EnemyPool enemyPool, Rect worldBounds, TextureAtlas atlas, Sound sndBullet, TextureAtlas atlas2) {
         this.enemyPool = enemyPool;
         this.worldBounds = worldBounds;
         this.sndBullet = sndBullet;
@@ -58,53 +87,106 @@ public class EnemiesEmitter {
         enemyMediumRegions = Regions.split(region1, 1, 2, 2);
         TextureRegion region2 = atlas.findRegion("enemy2");
         enemyBigRegions = Regions.split(region2, 1, 2, 2);
+        TextureRegion region3 = atlas2.findRegion("flat");
+        enemyPLANERegions = Regions.split(region3,1, 1, 1);
+        TextureRegion region4 = atlas2.findRegion("key");
+        enemySTARRegions = Regions.split(region4,1, 1, 1);
+        TextureRegion region5 = atlas2.findRegion("we");
+        enemyBIGPLANERegions = Regions.split(region5,1, 1, 1);
         bulletRegion = atlas.findRegion("bulletEnemy");
     }
 
-    public void generateEnemies(float deltaTime) {
+    public void setToNewGame() {
+        stage = 1;
+    }
+
+    public int getStage() {
+        return stage;
+    }
+
+    public void generateEnemies(float deltaTime, int frags) {
+        stage = frags / 10 + 1;
         generateTimer += deltaTime;
         if(generateTimer >= generateInterval) {
             generateTimer = 0f;
             Enemy enemy = enemyPool.obtain();
             float type = (float) Math.random();
-            if(type < 0.7f) {
+            if(type < 0.2f) {
                 enemy.set(
                         enemySmallRegions,
                         enemySmallV,
                         bulletRegion,
                         ENEMY_SMALL_BULLET_HEIGHT,
                         ENEMY_SMALL_BULLET_VY,
-                        ENEMY_SMALL_BULLET_DAMAGE,
+                        ENEMY_SMALL_BULLET_DAMAGE * stage,
                         ENEMY_SMALL_RELOAD_INTERVAL,
                         sndBullet,
                         ENEMY_SMALL_HEIGHT,
-                        ENEMY_SMALL_HP);
-            } else if(type < 0.9f) {
+                        ENEMY_SMALL_HP * stage);
+            } else if(type < 0.3f) {
                 enemy.set(
                         enemyMediumRegions,
                         enemyMediumV,
                         bulletRegion,
                         ENEMY_MEDIUM_BULLET_HEIGHT,
                         ENEMY_MEDIUM_BULLET_VY,
-                        ENEMY_MEDIUM_BULLET_DAMAGE,
+                        ENEMY_MEDIUM_BULLET_DAMAGE * stage,
                         ENEMY_MEDIUM_RELOAD_INTERVAL,
                         sndBullet,
                         ENEMY_MEDIUM_HEIGHT,
-                        ENEMY_MEDIUM_HP);
-            } else {
+                        ENEMY_MEDIUM_HP * stage);
+            } else if (type < 0.4f) {
                 enemy.set(
                         enemyBigRegions,
                         enemyBigV,
                         bulletRegion,
                         ENEMY_BIG_BULLET_HEIGHT,
                         ENEMY_BIG_BULLET_VY,
-                        ENEMY_BIG_BULLET_DAMAGE,
+                        ENEMY_BIG_BULLET_DAMAGE * stage,
                         ENEMY_BIG_RELOAD_INTERVAL,
                         sndBullet,
                         ENEMY_BIG_HEIGHT,
-                        ENEMY_BIG_HP);
+                        ENEMY_BIG_HP * stage);
+            } else if(type < 0.5f) {
+                enemy.set(
+                        enemyBIGPLANERegions,
+                        enemyBIGPLANEV,
+                        bulletRegion,
+                        ENEMY_BIGPLANE_BULLET_HEIGHT,
+                        ENEMY_BIGPLANE_BULLET_VY,
+                        ENEMY_BIGPLANE_BULLET_DAMAGE * stage,
+                        ENEMY_BIGPLANE_RELOAD_INTERVAL,
+                        sndBullet,
+                        ENEMY_BIGPLANE_HEIGHT,
+                        ENEMY_BIGPLANE_HP * stage);
+            } else if(type < 0.7f) {
+                enemy.set(
+                        enemyPLANERegions,
+                        enemyPLANEV,
+                        bulletRegion,
+                        ENEMY_PLANE_BULLET_HEIGHT,
+                        ENEMY_PLANE_BULLET_VY,
+                        ENEMY_PLANE_BULLET_DAMAGE * stage,
+                        ENEMY_PLANE_RELOAD_INTERVAL,
+                        sndBullet,
+                        ENEMY_PLANE_HEIGHT,
+                        ENEMY_PLANE_HP * stage);
+            }
+            else if(type < 0.9f) {
+                enemy.set(
+                        enemySTARRegions,
+                        enemySTARV,
+                        bulletRegion,
+                        ENEMY_STAR_BULLET_HEIGHT,
+                        ENEMY_STAR_BULLET_VY,
+                        ENEMY_STAR_BULLET_DAMAGE * stage,
+                        ENEMY_STAR_RELOAD_INTERVAL,
+                        sndBullet,
+                        ENEMY_STAR_HEIGHT,
+                        ENEMY_STAR_HP * stage);
             }
             enemy.pos.x = Rnd.nextFloat(worldBounds.getLeft() + enemy.getHalfWidth(), worldBounds.getRight() - enemy.getHalfWidth());
+//            enemy.pos.x = worldBounds.getLeft() + enemy.getHalfWidth();
             enemy.setBottom(worldBounds.getTop());
         }
     }
--- core/src/ru/geekbrains/java_games/common/enemies/Enemy.java
+++ core/src/ru/geekbrains/java_games/common/enemies/Enemy.java
@@ -1,25 +1,23 @@
 package ru.geekbrains.java_games.common.enemies;
 
 import com.badlogic.gdx.audio.Sound;
-import com.badlogic.gdx.graphics.g2d.ParticleEffectPool;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.java_games.common.Ship;
 import ru.geekbrains.java_games.common.bullets.BulletPool;
-import ru.geekbrains.java_games.common.explosions.Explosion;
 import ru.geekbrains.java_games.common.explosions.ExplosionPool;
 import ru.geekbrains.java_games.screens.game_screen.MainShip;
 import ru.geekuniversity.engine.math.Rect;
 
 public class Enemy extends Ship {
 
-//    private enum State { DESCENT, FIGHT }
+    private enum State { DESCENT, FIGHT }
 
-//    private final Vector2 descentV = new Vector2(0f, -0.15f);
+    private final Vector2 descentV = new Vector2(0f, -0.15f);
     private final Vector2 v0 = new Vector2();
     private final MainShip mainShip;
-//    private State state;
+    private State state;
 
     Enemy(BulletPool bulletPool, ExplosionPool explosionPool, Rect worldBounds, MainShip mainShip) {
         super(bulletPool, explosionPool, worldBounds);
@@ -50,44 +48,38 @@ public class Enemy extends Ship {
         this.hp = hp;
         setHeightProportion(height);
         reloadTimer = reloadInterval;
-        v.set(v0);
-//        state = State.DESCENT;
+        v.set(descentV);
+        state = State.DESCENT;
     }
 
     @Override
     public void update(float deltaTime) {
         super.update(deltaTime);
-//        switch (state) {
-//            case DESCENT:
-//                if(getTop() <= worldBounds.getTop()) {
-//                    v.set(v0);
-//                    state = State.FIGHT;
-//                }
-//                break;
-//            case FIGHT:
+        switch (state) {
+            case DESCENT:
+                if(getTop() <= worldBounds.getTop()) {
+                    v.set(v0);
+                    state = State.FIGHT;
+                }
+                break;
+            case FIGHT:
                 reloadTimer += deltaTime;
                 if (reloadTimer >= reloadInterval) {
                     reloadTimer = 0f;
                     shoot();
                 }
                 if(getBottom() < worldBounds.getBottom()) {
-//                    mainShip.damage(bulletDamage);
+                    mainShip.damage(bulletDamage);
                     boom();
                     destroy();
                 }
-                if(!isOutside(mainShip)) {
-                    boom();
-                    destroy();
-                }
-//                break;
-//            default:
-//                throw new RuntimeException("Unknown state = " + state);
-//        }
+                break;
+            default:
+                throw new RuntimeException("Unknown state = " + state);
+        }
     }
 
-
-//
-//    public boolean isBulletCollision (Rect bullet) {
-//        return !(bullet.getRight() < getLeft() || bullet.getLeft() > getRight() || bullet.getBottom() > getTop() || bullet.getTop() < pos.y);
-//    }
+    public boolean isBulletCollision (Rect bullet) {
+        return !(bullet.getRight() < getLeft() || bullet.getLeft() > getRight() || bullet.getBottom() > getTop() || bullet.getTop() < pos.y);
+    }
 }
